19e82893b54577be3dd0b15f82cc8a8a4b594401
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-05-29 05:15:06 +0700

    Update search-anime.php

--- search-anime.php
+++ search-anime.php
@@ -2,16 +2,12 @@
 require('listapikey.php');
 $apikey=rawurlencode($_GET['apikey']);
 $keyword = rawurlencode($_GET['key']);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_REFERER, 'https://api.jikan.moe/v3/search/anime?q='.$keyword.'&limit=10');
-curl_setopt($ch, CURLOPT_URL, 'https://api.jikan.moe/v3/search/anime?q='.$keyword.'&limit=10');
-$data = curl_exec($ch);
-curl_close($ch);
-$res = json_decode($data->raw_body, true);
+require_once('./unirest-php-master/src/Unirest.php');
+
+$uri= 'https://api.jikan.moe/v3/search/anime?q='.$keyword.'&limit=10';
+$respon = Unirest\Request::get("$uri");
+$res = json_decode($respon->raw_body, true);
+
 $result = array(
     'created by'=>'rzmy',
     'docs by'=>'jikan',

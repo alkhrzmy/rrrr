7ae9ca6c0564787ade3a1f5e92d1971bda0bbde5
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-05-28 22:15:24 +0700

    Update ongoing.php

--- ongoing.php
+++ ongoing.php
@@ -1,6 +1,5 @@
 <?php
-ini_set('display_errors',1); 
-error_reporting(E_ALL);
+
 require('listapikey.php');
 require('simple_html_dom.php');
 $apikey=rawurlencode($_GET['apikey']);
@@ -10,17 +9,18 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_REFERER, 'https://www.samehadaku.tv/');
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.samehadaku.tv');
 curl_setopt($ch, CURLOPT_URL, 'https://www.samehadaku.tv');
 $data = curl_exec($ch);
 curl_close($ch);
 $output = array();
+
 $html = str_get_html($data);
 
-$bahan = $html->find('ul[class=posts-items]',0)
-$kotak = $bahan->find('li[class=post-item');
+$bahan = $html->find('div[class=mag-box-container]',0)
+$kotak = $bahan->find('li[class=post-item]');
 
-echo $bahan;
+echo $kotak;
 
 foreach($kotak as $key => $val) {
   $title = $val->find('h3[class=post-title]',0)->find('a',0)->innertext;

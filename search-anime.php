36145abed9ce9171fc3b91159dcd015d3a0607d5
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-05-29 05:07:00 +0700

    Update search-anime.php

--- search-anime.php
+++ search-anime.php
@@ -11,9 +11,14 @@ curl_setopt($ch, CURLOPT_REFERER, 'https://api.jikan.moe/v3/search/anime?q='.$ke
 curl_setopt($ch, CURLOPT_URL, 'https://api.jikan.moe/v3/search/anime?q='.$keyword.'&limit=10');
 $data = curl_exec($ch);
 curl_close($ch);
+$res = json_decode($data->raw_body, true);
+$result = array(
+    'created by'=>'rzmy',
+    'result'=> $res['results']
+);
 
 if(in_array($apikey, $list) && $keyword){
-    echo $data;
+    echo json_encode($result, JSON_PRETTY_PRINT);
 }else{
   echo "maaf, input salah. Ex: <a href='./search-anime.php?key=hitori&apikey=contoh'>https://apirzmy.herokuapp.com/search-anime.php?key=hitori&apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a>";
 }

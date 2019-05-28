8be0555f6289d908d8000142a136b75d7aede01d
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-05-28 22:41:49 +0700

    Update ongoing.php

--- ongoing.php
+++ ongoing.php
@@ -17,7 +17,7 @@ $output = array();
 
 $html = str_get_html($data);
 
-$bahan = $html->find('div[class=container-wrapper]',0);
+$bahan = $html->find('ul[class=posts-items',1);
 $kotak = $bahan->find('li[class=post-item]');
 
 

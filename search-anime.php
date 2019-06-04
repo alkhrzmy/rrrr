07e4d8a89ab19b366eaba457ff6df57abb9bc3b6
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-04 22:40:47 +0700

    Update search-anime.php

--- search-anime.php
+++ search-anime.php
@@ -1,4 +1,18 @@
-<?php
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Anime search</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link href="https://apirzmy.000webhostapp.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <script src="https://apirzmy.000webhostapp.com/js/ie-emulation-modes-warning.js"></script>
+    </head>
+    <body>
+        
+        <?php
 require('listapikey.php');
 $apikey=rawurlencode($_GET['apikey']);
 $keyword = rawurlencode($_GET['key']);
@@ -24,3 +38,6 @@ if($keyword){
   echo "maaf, input salah. Ex: <a href='./search-anime.php?key=hitori&apikey=contoh'>https://apirzmy.herokuapp.com/search-anime.php?key=hitori&apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a>";
 }
 ?>
+        
+    </body>
+</html>

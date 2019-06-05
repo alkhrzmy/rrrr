63c2d497d13f44987a8a5c1b7dc70188f3a2027a
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-05 14:26:02 +0700

    Create error.php

---
+++ error.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Anime search</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link href="https://apirzmy.000webhostapp.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <script src="https://apirzmy.000webhostapp.com/js/ie-emulation-modes-warning.js"></script>
+    </head>
+    <body>
+
+<?php
+$halaman=rawurlencode($_GET['h']);
+$error = rawurlencode($_GET['e']);
+
+if($error=='nonev1'){
+  echo "<br/><div class='container'><div class='card bg-danger text-warning'>
+    <div class='card-body'>Parameter yang anda masukan salah</div></div><br/>
+  Maaf, input salah. Ex: <a href='./".$halaman."?key=hitori&apikey=contoh'>https://apirzmy.herokuapp.com/".$halaman."?key=hitori&apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a></div>";
+}elseif($error=='nonev2'){
+  echo "<br/><div class='container'><div class='card bg-danger text-warning'>
+    <div class='card-body'>Parameter yang anda masukan salah</div></div><br/>
+  Maaf, input salah. Ex: <a href='./".$halaman."?apikey=contoh'>https://apirzmy.herokuapp.com/".$halaman."?apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a></div>";
+}
+
+
+?>
+
+
+  </body>
+</html>

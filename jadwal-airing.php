1d994eb1c764ae48b10398591eb79214d49bdd62
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-05 13:26:53 +0700

    Update jadwal-airing.php

--- jadwal-airing.php
+++ jadwal-airing.php
@@ -1,4 +1,18 @@
-<?php
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Anime search</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link href="https://apirzmy.000webhostapp.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <script src="https://apirzmy.000webhostapp.com/js/ie-emulation-modes-warning.js"></script>
+    </head>
+    <body>
+        
+        <?php
 require('listapikey.php');
 $apikey=rawurlencode($_GET['apikey']);
 $keyword = rawurlencode($_GET['key']);
@@ -20,12 +34,22 @@ $result = array(
 );
 if(in_array($keyword,$map_day)){
   if(in_array($apikey, $list)){
-        echo json_encode($result, JSON_PRETTY_PRINT);
-    }else{
-        echo '<center><h2>Maaf, apikey tidak ditemukan. Silahkan kontak creator. <br/></h2><h1><a href="https://line.me/ti/p/~alkhoarizmy" color="green">LINE</a></h1></center>';
-    }
+    echo json_encode($result, JSON_PRETTY_PRINT);
+  }else{
+    echo "<br/><div class='container'><div class='card bg-danger text-red'>
+    <div class='card-body'>Parameter yang anda masukan salah</div></div><br/>Maaf, input salah. Ex: <a href='./jadwal-airing.php?key=sunday&apikey=contoh'>https://apirzmy.herokuapp.com/jadwal-airing.php?key=sunday&apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a></div>";
+  }
 }else{
-echo "Error: input salah atau apikey tidak ada\n
+echo "Error: input salah atau apikey tidak ada<br/>
 contoh => https://apirzmy.herokuapp.com/jadwal-airing.php?apikey=contoh&key=sunday";
 }
 ?>
+        
+    </body>
+</html>
+
+
+
+
+
+

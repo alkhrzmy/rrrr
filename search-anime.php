ca23c11b0ee98bd794949b02705d2f9c445be8bf
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-05 14:52:03 +0700

    Update search-anime.php

--- search-anime.php
+++ search-anime.php
@@ -1,18 +1,4 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Anime search</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <link href="https://apirzmy.000webhostapp.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-        <script src="https://apirzmy.000webhostapp.com/js/ie-emulation-modes-warning.js"></script>
-    </head>
-    <body>
-        
-        <?php
+<?php
 require('listapikey.php');
 $apikey=rawurlencode($_GET['apikey']);
 $keyword = rawurlencode($_GET['key']);
@@ -32,14 +18,9 @@ if($keyword){
     if(in_array($apikey, $list)){
         echo json_encode($result, JSON_PRETTY_PRINT);
     }else{
-        echo '<center><h2>Maaf, apikey tidak ditemukan. Silahkan kontak creator. <br/></h2><h1><a href="https://line.me/ti/p/~alkhoarizmy" color="green">LINE</a></h1></center>';
+        header('Location: https://apirzmy.herokuapp.com/error.php?e=nonev1.2&h=search-anime');
     }
 }else{
-  echo "<br/><div class='container'><div class='card bg-danger text-red'>
-    <div class='card-body'>Parameter yang anda masukan salah</div></div><br/>
-  Maaf, input salah. Ex: <a href='./search-anime.php?key=hitori&apikey=contoh'>https://apirzmy.herokuapp.com/search-anime.php?key=hitori&apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a></div>";
+ header('Location: https://apirzmy.herokuapp.com/error.php?e=nonev1&h=search-anime');
 }
 ?>
-        
-    </body>
-</html>

e4a3756dc0972f0335f3e798c26f963f6066008c
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-05 14:26:35 +0700

    Update jadwal-airing.php

--- jadwal-airing.php
+++ jadwal-airing.php
@@ -1,18 +1,4 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Anime search</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <link href="https://apirzmy.000webhostapp.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-        <script src="https://apirzmy.000webhostapp.com/js/ie-emulation-modes-warning.js"></script>
-    </head>
-    <body>
-        
-        <?php
+<?php
 require('listapikey.php');
 $apikey=rawurlencode($_GET['apikey']);
 $keyword = rawurlencode($_GET['key']);
@@ -45,9 +31,6 @@ echo "<br/><div class='container'><div class='card bg-danger text-red'>
 contoh => https://apirzmy.herokuapp.com/jadwal-airing.php?apikey=contoh&key=sunday</div>";
 }
 ?>
-        
-    </body>
-</html>
 
 
 

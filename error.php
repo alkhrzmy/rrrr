7d6386eda8cdcc052800c24b5e7e2ed6264100bf
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-05 14:49:49 +0700

    Update error.php

--- error.php
+++ error.php
@@ -24,6 +24,10 @@ if($error=='nonev1'){
   echo "<br/><div class='container'><div class='card bg-danger text-warning'>
     <div class='card-body'>Parameter yang anda masukan salah</div></div><br/>
   Maaf, input salah. Ex: <a href='./".$halaman.".php?apikey=contoh'>https://apirzmy.herokuapp.com/".$halaman.".php?apikey=contoh</a><br/>untuk mendapatkan apikey silahkan kontak creator atau langsung <a href='http://line.me/ti/p/~alkhoarizmy'>Klik disini</a></div>";
+}elseif($error=='nonev1.2'){
+    echo '<center><h2>Maaf, apikey tidak ditemukan. Silahkan kontak creator. <br/></h2><h1><a href="https://line.me/ti/p/~alkhoarizmy" color="green">LINE</a></h1></center>';
+}elseif($error=='nonev2.2){
+    echo '<center><h2>Maaf, apikey tidak ditemukan. Silahkan kontak creator. <br/></h2><h1><a href="https://line.me/ti/p/~alkhoarizmy" color="green">LINE</a></h1></center>';
 }
 
 

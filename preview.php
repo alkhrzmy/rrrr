4f3c6f86bc0bc27b424fc4a22b0ea1d5739b9731
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-06 14:22:45 +0700

    Update preview.php

--- preview.php
+++ preview.php
@@ -11,7 +11,9 @@
 
 <?php
 $p = rawurlencode($_GET['p']);
-echo <<<EXCERPT
+$type = rawurlencode($_GET['type']);
+if($type==2){
+  echo <<<EXCERPT
 <div class="container">
   <h3>Documentation</h3>
   <br>
@@ -63,6 +65,55 @@ echo <<<EXCERPT
 </div>
 
 EXCERPT;
+}elseif($type==1){
+  echo <<<EXCERPT
+<div class="container">
+  <h3>Documentation</h3>
+  <br>
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="tab" href="#home">URL+Parameters</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#menu1">Example</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div id="home" class="container tab-pane active"><br>
+      <h4>URL+Parameters</h4>
+      <blockquote class="blockquote">
+      <span class="badge badge-success">URL</span><font size="3">https://apirzmy.herokuapp.com/$p</font>
+      </blockquote><br/>
+      <table class="table">
+    <thead>
+      <tr class="table-primary">
+        <th>Parameter</th>
+        <th>Required</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>apikey</th>
+        <th>yes</th>
+        <th>untuk mendapatkan APIkey dapat menghubungi <a href="line.me/ti/p/~alkhoarizmy">creator</a></th>
+      </tr>
+    </tbody>
+  </table>
+    </div>
+    <div id="menu1" class="container tab-pane fade"><br>
+      <h4>Example</h4>
+      
+    </div>
+  </div>
+  
+</div>
+
+EXCERPT;
+}
 
 ?>
 

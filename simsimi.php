994d98a8cdee29fb3df59969308f1f094ea2d148
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-23 20:52:17 +0700

    Update simsimi.php

--- simsimi.php
+++ simsimi.php
@@ -1,39 +1,33 @@
 <?php
-$headers = array(
-  "User-Agent" => "Dalvik/2.1.0 (Linux; U; Android 6.0.1; ASUS_A007 Build/MMB29P)",
-  "Host" => "secureapp.simsimi.com",
-  "Connection" => "Keep-Alive",
-  "Accept-Encoding" => "gzip"
+require('listapikey.php');
+$apikey=rawurlencode($_GET['apikey']);
+$keyword = rawurlencode($_GET['key']);
+require_once('./unirest-php-master/src/Unirest.php');
+
+$uri = "https://secureapp.simsimi.com/v1/simsimi/talkset?uid=271898762&av=6.8.7.1&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=alofKTlYS08reLe4gzCD2W7mijs%3D&message_sentence=".$keyword."&normalProb=2&isFilter=1&talkCnt=1&talkCntTotal=1&reqFilter=0&session=udu6auK1LfyAQwLzupMPbdKVYDX9HkLnkYkU4jPWFmX6MysmRf6uFqg5RhqqVzVjKixAJaoNuxLi1YNFjLrs2Qu3&triggerKeywords=%5B%5D";
+
+$respon = Unirest\Request::get("$uri");
+$res = json_decode($respon->raw_body, true);
+$result = array(
+    'created by'=>'rzmy',
+    'origin' => 'secureapp.simsimi.com',
+    'result'=> array(
+      'text'=>$res['simsimi_talk_set']['answers'][0]['origin_answerSentence'],
+      'timestamp'=>$res['simsimi_talk_set']['answers'][0]['timestamp']
+    ),
 );
-
-$url = "https://secureapp.simsimi.com/v1/simsimi/talkset?uid=271898762&av=6.8.7.1&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=alofKTlYS08reLe4gzCD2W7mijs%3D&message_sentence=aku%20suka%20loli&normalProb=2&isFilter=1&talkCnt=1&talkCntTotal=1&reqFilter=0&session=udu6auK1LfyAQwLzupMPbdKVYDX9HkLnkYkU4jPWFmX6MysmRf6uFqg5RhqqVzVjKixAJaoNuxLi1YNFjLrs2Qu3&triggerKeywords=%5B%5D";
-
-$data = array();
-
-$context = stream_context_create(array(
-  'http' => array(
-    'method' => 'POST',
-    'header' => implode('/r/n', $headers),
-    'content' => $data
-  ),
-));
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POST, 1 );
-curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_HTTPHEADER, json_encode($headers));
-curl_setopt($ch, CURLOPT_URL, $url);
-$results = curl_exec($ch);
-curl_close($ch);
-
-echo $results;
-
+$err = array(
+    'error'=>true,
+    'msg'=>'Maaf, parameter input yang anda masukan salah. Ex: ./simsimi.php?apikey=sbeve&key=aku%20hamil'
+);
+if($keyword){
+    if(in_array($apikey, $list)){
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }else{
+        echo json_encode($err, JSON_PRETTY_PRINT);
+    }
+}else{
+ echo json_encode($err, JSON_PRETTY_PRINT);
+}
 
 ?>
